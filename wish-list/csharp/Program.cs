f040b20573d517670fabc34a4aabd9befad36104
Meowph <bpardue33@gmail.com>
2024-07-22 10:24:49 -0400

    completed wish-list

---
+++ wish-list/csharp/Program.cs
@@ -0,0 +1,20 @@
+
+using System;
+//idiomatic structure
+
+namespace Wish
+{
+  class Program
+
+  {
+    static void Main(string[] args)
+    {
+     List<string> wishes = new List<string> { "An original eight track of William Shatner's 'Lucy in the Sky with Diamonds'", "The ability to think of interesting wishes on short notice", "A washtub of Skittles", "World peace, but not the kind of world peace where all the humans are removed from existence", "A TV that is NOT internet-capable", "The ability to fly, but, like, really fast...not just, like, how fast I can walk, but, like, fast, you know?"};
+
+  Console.Write("My wishes...");
+  foreach (string wish in wishes) {
+    Console.Write(wish);
+  }
+    }
+  }
+}
